fa45a370495565cedbbf7c5548e93809278cb028
shivam2411 <f2013840@pilani.bits-pilani.ac.in>
2020-05-21 23:32:04 +0530

    Added project via spring initializr

---
+++ file-upload-platform/src/main/java/com/upload/fileuploadplatform/FileUploadPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.upload.fileuploadplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadPlatformApplication.class, args);
+	}
+
+}
---
+++ file-upload-platform/src/test/java/com/upload/fileuploadplatform/FileUploadPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upload.fileuploadplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
